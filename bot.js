c28f523ea959842f3244042b716369309c051156
Joe P Shoulak <joe@squarecompass.io>
2022-10-29 12:06:23 -0500

    to be continued

--- bot.js
+++ bot.js
@@ -31,7 +31,9 @@ const rest = new REST({ version: "10" }).setToken(process.env.BOT_TOKEN);
 
 /* == PART 2 == */
 const { Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -39,6 +41,9 @@ client.on("ready", () => {
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
+  if (interaction.channelId != "1035716879131619428") {
+    return await interaction.reply("This bot is not allowed in this channel");
+  }
 
   if (interaction.commandName === "d20") {
     const num = Math.ceil(Math.random() * 20);
